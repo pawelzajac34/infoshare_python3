e0987902646bb32350aa868e3572c7c879ce2082
Pawel <pawelzajacp8@gmail.com>
2018-06-04 20:19:56 +0200

    check whether

---
+++ day_3/zadanie2.py
@@ -0,0 +1,8 @@
+# TODO: mapisz program ktory sprawdzi czy liczba jest parzysta
+# TODO: liczba podawana przez uzytkownika
+
+number = int(input('Podaj dowolna liczbe: '))
+if number % 2 == 0:
+    print('liczba parzysta')
+else:
+    print('liczba nieparzysta')
