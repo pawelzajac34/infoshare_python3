58f30a931c2a9537973998633938aee3c0afbb67
Pawel <pawelzajacp8@gmail.com>
2018-06-06 11:17:17 +0200

    First homework. Use of the code ASCII.

---
+++ praca_domowa1.py
@@ -0,0 +1,7 @@
+# output: "Arek woli psy, a najbardziej boksery"
+x = "Ala ma kota, kot ma Ale"
+print(x[0] + chr(114) + x[-1] + x[7], chr(119) + x[8] + x[1] + chr(105),
+      chr(112) + chr(115) + chr(121) + x[11], x[10], chr(110) + x[2] +
+      chr(106) + chr(98) + x[18] + chr(114) + chr(100) + chr(122) + chr(105)
+      + x[-1] + chr(106), chr(98) + x[8] + x[7] + chr(115) + x[-1] +
+      chr(114) + chr(121))
